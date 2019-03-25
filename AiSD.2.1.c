4f6049b5a942021730c13b4b6e8dd32a9f9e8b14
AiSDPM <48443641+AiSDPM@users.noreply.github.com>
2019-03-25 18:12:17 +0100

    Zakończone zad1 dla BST

--- AiSD.2.1.c
+++ AiSD.2.1.c
@@ -2,7 +2,6 @@
 
 struct tree { //inicjacja drzewa
 	int info;
-	struct tree *prev;
 	struct tree *left;
 	struct tree *right;
 };
@@ -16,7 +15,7 @@ struct tree *insert(struct tree *root, int x) { //budowa drzewa
 		return(root);
 	}
 	if(root->info > x)
-	     root->left = insert(root->left,x); 
+	     root->left = insert(root->left,x);
 	else {
 		if(root->info < x)
 			root->right = insert(root->right,x);
@@ -24,37 +23,49 @@ struct tree *insert(struct tree *root, int x) { //budowa drzewa
 	return(root);
 }
 
-struct tree *search(struct tree *root, int x) { //przeszukiwanie, trzeba poprawiæ
+ struct tree *search(struct tree *root, int x) {
 	struct tree *ptr;
 	ptr=root;
 	while(ptr) {
 		if(x>ptr->info)
-		     ptr=ptr->right; 
+		     ptr=ptr->right;
 		else if(x<ptr->info)
-		     ptr=ptr->left; 
+		     ptr=ptr->left;
 		else
 		    break;
 	}
+	return ptr;
+ }
+
+
+void postorder(struct tree *root) {
+	if(root != NULL) {
+		postorder(root->left);
+		postorder(root->right);
+		printf(" %d",root->info);
+		free(root);
+	}
+	return;
+}
+
 
 int main(void){
-	int tab1[] = {1, 4, 3, 5, 9, 7, 2, 8, 6, 0, 11, 10, 14, 17, 13, 15, 19, 12, 18, 16};
-	int i;
-	struct tree *root;
-	struct tree *BST;
-	BST= new struct tree;
-	root = BST;
-	BTS->info = tab1[0];
-	BTS->left = NULL;
-	BTS->right = NULL;
-	BTS->prev = root;
-		
-	for(i = 0; i < 20, i++){
-		BST= new struct tree;
-		BTS->info = tab1[i];
-		BTS->left = NULL;
-		BTS->right = NULL;	
-		BTS->prev =insert(BTS, tab[i]);
+	int tab1[] = {10, 4, 3, 5, 9, 7, 2, 8, 6, 0, 11, 1, 14, 17, 13, 15, 19, 12, 18, 16};
+	int i,x;
+	struct tree *a= NULL;
+	struct tree *BST = NULL;
+
+	for(i = 0; i < 20; i++){ //budowa drzewa
+		BST = insert(BST,tab1[i]);
 	}
 
+	for(i = 0; i < 20; i++){ // test search
+		a = search(BST,tab1[i]);
+		x= a->info;
+		printf("%d \n", x);
+	}
+
+    postorder(BST); // usuwanie
 	return 0;
 }
+
