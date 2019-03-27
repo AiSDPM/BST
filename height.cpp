64f207e4ef280c8482271976a141361649dc3769
Michal <pm.aisd.pp@gmail.com>
2019-03-27 17:10:10 +0100

    Zostalo dodac kilka rzeczy

---
+++ height.cpp
@@ -0,0 +1,97 @@
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+
+struct tree { //inicjacja drzewa
+	int info;
+	struct tree *left;
+	struct tree *right;
+};
+
+struct tree* newElement(int x)
+{
+    struct tree *root =(struct tree*)malloc(sizeof(struct tree));
+    root->info = x;
+    root->left = NULL;
+    root->right = NULL;
+    return root;
+}
+
+struct tree* insert(struct tree *root, int x) { //budowa drzewa
+	if(!root) {
+		root = newElement(x);
+		return(root);
+	}
+	if(root->info > x)
+	     root->left = insert(root->left,x);
+	else {
+		if(root->info < x)
+			root->right = insert(root->right,x);
+	}
+	return(root);
+}
+
+void deleteTree(struct tree *root) {
+	if(root != NULL) {
+		deleteTree(root->left);
+		deleteTree(root->right);
+		printf(" %d",root->info);
+		free(root);
+	}
+	return;
+}
+
+
+int height (struct tree *root, int h,int max)
+{
+	if(root != NULL) {
+		h++;
+		max = height(root->left, h, max);
+		max = height(root->right, h, max);
+		h--;
+	}
+	else
+	{
+
+		if (h>max)
+		  max=h;
+	}
+	return (max);
+}
+
+struct tree *makeAVL(struct tree *root, struct tree *node,int l, int p, int *tab) {
+    if (l>p)
+        return root;
+    int mid = (l+p)/2;
+    node = newElement(tab[mid]);
+    node->left = makeAVL(root, node, l, mid-1,tab);
+	node->right = makeAVL(root,node, mid+1, p ,tab);
+
+}
+
+
+
+int main(void){
+	int tab1[] = {10, 4, 3, 5, 9, 7, 2, 8, 6, 0, 11, 1, 14, 17, 13, 15, 19, 12, 18, 16};
+	int i,x;
+	struct tree *BST = NULL;
+
+	for(i = 0; i < 20; i++){ //budowa drzewa
+		BST = insert(BST,tab1[i]);
+	}
+
+	x= height(BST,0,0);
+    deleteTree(BST); // usuwanie
+    free(BST);
+
+    struct tree *AVL = NULL;
+    int len = sizeof(tab1)/sizeof(tab1[0]);
+    std::sort(tab1, tab1+ len);
+
+    AVL = makeAVL(AVL,AVL, 0, len-1, tab1);
+    x= height(AVL,0,0);
+	deleteTree(AVL); // usuwanie
+    free(AVL);
+
+	return 0;
+}
---
+++ tree.cpp
@@ -0,0 +1,72 @@
+#include <cstdio>
+#include <cstdlib>
+
+struct tree { //inicjacja drzewa
+	int info;
+	struct tree *left;
+	struct tree *right;
+};
+
+struct tree *insert(struct tree *root, int x) { //budowa drzewa
+	if(!root) {
+		root=(struct tree*)malloc(sizeof(struct tree));
+		root->info = x;
+		root->left = NULL;
+		root->right = NULL;
+		return(root);
+	}
+	if(root->info > x)
+	     root->left = insert(root->left,x);
+	else {
+		if(root->info < x)
+			root->right = insert(root->right,x);
+	}
+	return(root);
+}
+
+ struct tree *search(struct tree *root, int x) {
+	struct tree *ptr;
+	ptr=root;
+	while(ptr) {
+		if(x>ptr->info)
+		     ptr=ptr->right;
+		else if(x<ptr->info)
+		     ptr=ptr->left;
+		else
+		    break;
+	}
+	return ptr;
+ }
+
+
+void postorder(struct tree *root) {
+	if(root != NULL) {
+		postorder(root->left);
+		postorder(root->right);
+		printf(" %d",root->info);
+		free(root);
+	}
+	return;
+}
+
+
+int main(void){
+	int tab1[] = {10, 4, 3, 5, 9, 7, 2, 8, 6, 0, 11, 1, 14, 17, 13, 15, 19, 12, 18, 16};
+	int i,x;
+	struct tree *a= NULL;
+	struct tree *BST = NULL;
+
+	for(i = 0; i < 20; i++){ //budowa drzewa
+		BST = insert(BST,tab1[i]);
+	}
+
+	for(i = 0; i < 20; i++){ // test search
+		a = search(BST,tab1[i]);
+		x= a->info;
+		printf("%d \n", x);
+	}
+
+    postorder(BST); // usuwanie
+	return 0;
+}
+
