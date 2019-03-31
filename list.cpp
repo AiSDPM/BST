900f4e4ee0ec14f46b40351e15eab6fb71c90387
pgleska <piotr.gleska@wp.pl>
2019-03-31 15:15:17 +0200

    lista

--- list.cpp
+++ list.cpp
@@ -1,6 +1,11 @@
 #include <cstdio>
 #include <cstdlib>
+#include <iostream>
 #include <chrono>
+#include <time.h>
+#include <fstream>
+
+using namespace std;
 
 struct list{ // stryktura
 	int key;
@@ -43,7 +48,7 @@ void deleteList(struct list *start) {
     while(currentList != NULL)
 	{
 	    start = currentList -> next;
-	    printf(" %d",currentList->key);
+	   // printf(" %d",currentList->key);
 	    free(currentList);
 	    currentList = start;
 	}
@@ -63,19 +68,52 @@ struct list *search(struct list *start, int x) { //budowa drzewa
 
 int main (void)
 {
+    time_t start, koniec;
+    double roznica;
     int tab1[] = {10, 4, 3, 5, 9, 7, 2, 8, 6, 0, 11, 1, 14, 17, 13, 15, 19, 12, 18, 16};
-	int i,x;
-	struct list *Lista = NULL;
+	int i,x, petla, y=3000;
+	fstream plik, pliksearch,plikdelete;
+
+	struct list *Lista = NULL, Lista2;
 	struct list *a =NULL;
 
+    plik.open("wynikilista.txt", ios::out | ios::app); //dopisywanie wyników, tworzy nowy plik jesli brak
+    pliksearch.open("searchlista.txt", ios::out | ios::app);
+    plikdelete.open("deletelista.txt", ios::out | ios::app);
+
+    time( & start );
+    for (petla = 0; petla < y; petla++){
 	for(i = 0; i < 20; i++){ //budowa listy
-		Lista = insert(Lista,tab1[i]);
+            Lista = insert(Lista,tab1[i]);
+        }
 	}
-    for(i = 0; i < 20; i++){ // test search
-		a = search(Lista,tab1[i]);
-		x= a->key;
-		printf("%d \n", x);
+	time( & koniec );
+    roznica = difftime( koniec, start );
+    cout << "Obliczenia zajely Ci: " << roznica/y << " sekund\n";
+    plik << roznica << "     " << roznica/y << " y to " << y << endl;
+    plik.close();
+
+    time( & start );
+    for (petla = 0; petla < y; petla++){
+        for(i = 0; i < 20; i++){ // test search
+            a = search(Lista,tab1[i]);
+            x= a->key;
+            //printf("%d \n", x);
+        }
 	}
-	deleteList(Lista);
+	time( & koniec );
+    roznica = difftime( koniec, start );
+    cout << "Obliczenia zajely Ci: " << roznica/y << " sekund\n";
+    pliksearch << roznica << "     " << roznica/y << " y to " << y << endl;
+    pliksearch.close();
+
+	time( & start );
+    deleteList(Lista);
+    time( & koniec );
+    roznica = difftime( koniec, start );
+    cout << "Obliczenia zajely Ci: " << roznica/y << " sekund\n";
+    plikdelete << roznica << "     " << roznica/y << " y to " << y << endl;
+    plikdelete.close();
+
 	return 0;
 }
